7d08338a11379aa590ed9df993aeee7e8bc8eb3f
vladikbarabaye <brawlerkot336@gmail.com>
2025-04-16 00:27:19 +0300

    main

---
+++ hw/hw03/VladSusukailo/main.py
@@ -0,0 +1,18 @@
+from dataclasses import replace
+
+text = "Beautiful is better than ugly.".upper()
+word_of_text = []
+word = ""
+for i in text:
+    if i == " ":
+        word_of_text.append(word)
+        word = ""
+    word += i
+
+print(word_of_text)
+
+print(word_of_text.count(" BETTER"))
+print(word_of_text.count(" IS"))
+print(word_of_text.count(" NEVER"))
+replace_text = text.replace("I", "&")
+print(replace_text)
