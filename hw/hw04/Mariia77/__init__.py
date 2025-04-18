be24a0e6281e5bc788193c664f9138c8fcb8c1dd
Mariia Malyhina <mariia.malyhina@monobank.ua>
2025-04-18 23:06:30 +0300

    HW4

--- hw/hw04/Mariia77/__init__.py
+++
@@ -1,9 +0,0 @@
-def celsius_to_fahrenheit():
-    temp = float(input("Enter a temperature in Celsius: "))
-    if float(temp) < -273.15:
-        print("Error: Temperature below absolute zero (-273.15°C)")
-    else:
-        F = (temp * 9 / 5) + 32
-        print(f"{temp}°C is equivalent to {F}°F")
-celsius_to_fahrenheit()
-
