6e2a1e9f4d5dbdd5c3a733b1cce09115f3568cc0
Grytsun <grytsunmaryna@gmail.com>
2025-04-23 23:05:12 +0300

    added hw03 Grytsun

--- hw/hw03/Grytsun/03 Practical.py
+++ hw/hw03/Grytsun/03 Practical.py
@@ -1,3 +1,4 @@
+# 03-1
 text = """
 Sparse is better than dense.
 Readability counts.
@@ -15,3 +16,33 @@ print("\nUppercase:\n", text_upper)
 # Заміна "i" на "&"
 text_replaced = text.replace("i", "&")
 print("\nWith replaced 'i':\n", text_replaced)
+
+
+# 03-2
+number = int(input("Your number: "))
+
+# Добуток цифр
+digits = [int(d) for d in str(number)]
+product = 1
+for d in digits:
+    product *= d
+print("Product of digits: ", product)
+
+# У зворотному порядку
+reversed_number = int(str(number)[::-1])
+print("Reversed number: ", reversed_number)
+
+# У порядку зростання
+sorted_number = sorted(digits)
+print("Sorted number: ", sorted_number)
+
+#03-3
+# Заміна двох значень
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+# Поміняємо значення місцями без використання третьої змінної
+a, b = b, a
+
+print(f"After swapping: a = {a}, b = {b}")
+
