060c54356c659706ce48d6ac868382d7e0295ad7
Lera Pridorohina <theatra.quirrel@gmail.com>
2025-05-28 15:15:59 +0300

    Added hw11.

--- hw/hw11/theatra/hw_11.py
+++ hw/hw11/theatra/hw_11.py
@@ -1,3 +1,5 @@
+# Task 1
+
 def odd_even_age(input_age):
     if input_age < 0:
         raise Exception("Age must be positive!")
@@ -12,3 +14,35 @@ try:
     odd_even_age(age)
 except ValueError as e:
     print("Please enter a number!")
+
+
+# Task 2
+
+def day_of_week(number):
+    try:
+        number = int(number)
+        match number:
+            case "1":
+                return "Monday"
+            case "2":
+                return "Tuesday"
+            case "3":
+                return "Wednesday"
+            case "4":
+                return "Thursday"
+            case "5":
+                return "Friday"
+            case "6":
+                return "Saturday"
+            case "7":
+                return "Sunday"
+            case _:
+                print("Only numbers 1 to 7 are allowed!")
+    except ValueError as e:
+       print("Must be a number!")
+
+
+day_number = input("Please enter the day number here: ")
+day_of_week(day_number)
+
+
